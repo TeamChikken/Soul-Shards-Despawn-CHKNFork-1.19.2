e69c520080c1b9e598ca925a04d4d8d10f0721ee
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-03-27 20:20:55 +0100

    fix: more api issues

--- src/main/java/info/tehnut/soulshards/core/util/EnchantmentSoulStealer.java
+++ src/main/java/info/tehnut/soulshards/core/util/EnchantmentSoulStealer.java
@@ -1,22 +1,22 @@
 package info.tehnut.soulshards.core.util;
 
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentTarget;
-import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 public class EnchantmentSoulStealer extends Enchantment {
 
     public EnchantmentSoulStealer() {
-        super(Weight.UNCOMMON, EnchantmentTarget.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
+        super(Rarity.UNCOMMON, EnchantmentCategory.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
     }
 
     @Override
-    public int getMinimumPower(int level) {
+    public int getMinCost(int level) {
         return (level - 1) * 11;
     }
 
     @Override
-    public int getMaximumLevel() {
+    public int getMaxLevel() {
         return 5;
     }
 }
