968f03023a84d40374b3edfc0bd2328350c61a23
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-03-28 01:12:09 +0100

    fix: mod init

--- src/main/java/info/tehnut/soulshards/SoulShardsClient.java
+++ src/main/java/info/tehnut/soulshards/SoulShardsClient.java
@@ -1,14 +1,15 @@
 package info.tehnut.soulshards;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import info.tehnut.soulshards.core.RegistrarSoulShards;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.renderer.RenderType;
 
 public class SoulShardsClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(RegistrarSoulShards.SOUL_CAGE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(RegistrarSoulShards.SOUL_CAGE, RenderType.cutout());
     }
 }
