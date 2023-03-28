68192c019dfea1f940c651247fbcde44870876ef
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-03-28 15:16:06 +0100

    chore: switch to quilt entry points

--- src/main/java/info/n4tomic/soulshards/SoulShards.java
+++ src/main/java/info/n4tomic/soulshards/SoulShards.java
@@ -6,20 +6,23 @@ import info.n4tomic.soulshards.core.EventHandler;
 import info.n4tomic.soulshards.core.RegistrarSoulShards;
 import info.n4tomic.soulshards.core.data.Tier;
 import info.n4tomic.soulshards.core.util.JsonUtil;
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.Registry;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.GameRules;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.QuiltLoader;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
 import java.io.File;
 
 public class SoulShards implements ModInitializer {
 
     public static final String MODID = "soulshards";
-    public static final ConfigSoulShards CONFIG = JsonUtil.fromJson(TypeToken.get(ConfigSoulShards.class), new File(FabricLoader.getInstance().getConfigDirectory(), MODID + "/" + MODID + ".json"), new ConfigSoulShards());
+    public static ConfigSoulShards CONFIG = JsonUtil.fromJson(TypeToken.get(ConfigSoulShards.class),
+            new File(QuiltLoader.getConfigDir().toFile(), MODID + "/" + MODID + ".json"),
+            new ConfigSoulShards());
     public static EntityDataAccessor<Boolean> cageBornTag;
     public static GameRules.Key<GameRules.BooleanValue> allowCageSpawns;
     public static final String BOSS_TAG = "c:bosses";
--- src/main/java/info/n4tomic/soulshards/SoulShardsClient.java
+++ src/main/java/info/n4tomic/soulshards/SoulShardsClient.java
@@ -1,12 +1,12 @@
 package info.n4tomic.soulshards;
 
 import info.n4tomic.soulshards.core.RegistrarSoulShards;
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.renderer.RenderType;
+import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.loader.impl.util.log.Log;
 import org.quiltmc.loader.impl.util.log.LogCategory;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 public class SoulShardsClient implements ClientModInitializer {
 
