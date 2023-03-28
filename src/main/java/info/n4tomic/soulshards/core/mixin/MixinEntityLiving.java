fc2f0db7b206524790ee1b72b689776627bfd5b0
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-03-28 13:10:15 +0100

    fix: error on mob spawn

--- src/main/java/info/n4tomic/soulshards/core/mixin/MixinEntityLiving.java
+++ src/main/java/info/n4tomic/soulshards/core/mixin/MixinEntityLiving.java
@@ -6,8 +6,15 @@ import info.n4tomic.soulshards.core.EventHandler;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.quiltmc.loader.impl.util.log.Log;
+import org.quiltmc.loader.impl.util.log.LogCategory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,10 +31,14 @@ public class MixinEntityLiving {
 
     @Inject(method = "defineSynchedData", at = @At("TAIL"))
     private void initDataTracker(CallbackInfo callbackInfo) {
-        LivingEntity entity = (LivingEntity) (Object) this;
-        if (entity instanceof Player)
-            return;
-        entity.getEntityData().set(SoulShards.cageBornTag, false);
+        try {
+            LivingEntity entity = (LivingEntity) (Object) this;
+            if (entity instanceof Player)
+                return;
+            entity.getEntityData().define(SoulShards.cageBornTag, false);
+        } catch(Exception e) {
+            Log.error(LogCategory.GENERAL, "during synched data: %s", e.getMessage());
+        }
     }
 
     @Inject(method = "die", at = @At("HEAD"))
