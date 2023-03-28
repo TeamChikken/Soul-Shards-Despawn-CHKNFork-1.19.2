c769df8e1391cde3b24ba10be3b84fc390c8ef54
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-03-28 01:02:19 +0100

    fix: soul bar rendering

--- src/main/java/info/tehnut/soulshards/core/mixin/client/MixinItemRenderer.java
+++ src/main/java/info/tehnut/soulshards/core/mixin/client/MixinItemRenderer.java
@@ -1,11 +1,15 @@
 package info.tehnut.soulshards.core.mixin.client;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
 import info.tehnut.soulshards.SoulShards;
 import info.tehnut.soulshards.core.data.Binding;
 import info.tehnut.soulshards.core.data.Tier;
 import info.tehnut.soulshards.item.ItemSoulShard;
+import net.minecraft.client.renderer.FaceInfo;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.world.item.ItemStack;
+import org.apache.logging.log4j.core.pattern.TextRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -35,24 +39,24 @@ public abstract class MixinItemRenderer {
 
         RenderSystem.disableDepthTest();
         RenderSystem.disableTexture();
-        RenderSystem.disableAlphaTest();
+        RenderSystem.disableCull();
         RenderSystem.disableBlend();
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder buffer = tessellator.getBuffer();
-        buffer.begin(7, VertexFormats.POSITION_COLOR);
+        Tesselator tessellator = Tesselator.getInstance();
+        BufferBuilder buffer = tessellator.getBuilder();
+        buffer.begin(VertexFormat.Mode.LINE_STRIP, DefaultVertexFormat.POSITION_COLOR);
         prepareQuad(buffer, x + 2, y + 13, 13, 2, 0, 0, 0);
         prepareQuad(buffer, x + 2, y + 13, (int) (percentage * 13), 1, color >> 16 & 255, color >> 8 & 255, color & 255);
-        Tessellator.getInstance().draw();
+        Tesselator.getInstance().end();
         RenderSystem.enableBlend();
-        RenderSystem.enableAlphaTest();
+        RenderSystem.enableCull();
         RenderSystem.enableTexture();
         RenderSystem.enableDepthTest();
     }
 
     private static void prepareQuad(BufferBuilder buffer, int x, int y, int width, int height, int r, int g, int b) {
-        buffer.vertex(x, y, 0.0D).color(r, g, b, 255).next();
-        buffer.vertex(x, y + height, 0.0D).color(r, g, b, 255).next();
-        buffer.vertex(x + width, y + height, 0.0D).color(r, g, b, 255).next();
-        buffer.vertex(x + width, y, 0.0D).color(r, g, b, 255).next();
+        buffer.vertex(x, y, 0.0D).color(r, g, b, 255).endVertex();
+        buffer.vertex(x, y + height, 0.0D).color(r, g, b, 255).endVertex();
+        buffer.vertex(x + width, y + height, 0.0D).color(r, g, b, 255).endVertex();
+        buffer.vertex(x + width, y, 0.0D).color(r, g, b, 255).endVertex();
     }
 }
