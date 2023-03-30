b892b34ec5765c12498e0f4a86253be4a3f2b85f
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-03-30 19:30:45 +0100

    feat: add config screen for forge

--- forge/src/main/java/info/x2a/soulshards/forge/SoulShardsModForge.java
+++ forge/src/main/java/info/x2a/soulshards/forge/SoulShardsModForge.java
@@ -1,25 +1,17 @@
 package info.x2a.soulshards.forge;
 
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import dev.architectury.platform.forge.EventBuses;
 import dev.architectury.registry.client.rendering.RenderTypeRegistry;
 import info.x2a.soulshards.SoulShards;
 import info.x2a.soulshards.compat.clothconfig.SoulShardsConfigScreen;
 import info.x2a.soulshards.core.registry.RegistrarSoulShards;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraftforge.client.ClientCommandHandler;
-import net.minecraftforge.client.ClientCommandSourceStack;
-import net.minecraftforge.client.event.RegisterClientCommandsEvent;
-import net.minecraftforge.eventbus.EventBus;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-import static com.mojang.brigadier.builder.LiteralArgumentBuilder.literal;
 
 @Mod(SoulShards.MODID)
 public class SoulShardsModForge {
@@ -31,5 +23,10 @@ public class SoulShardsModForge {
             RenderTypeRegistry.register(RenderType.cutout(), RegistrarSoulShards.SOUL_CAGE.get());
         });
         SoulShards.initCommon();
+        if (SoulShards.IS_CLOTH_CONFIG_LOADED) {
+            ModLoadingContext.get()
+                             .registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> new SoulShardsConfigScreen(screen).screen()));
+        }
+
     }
 }
